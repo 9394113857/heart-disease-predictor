b727500916715ebcfa703a6cc7bd6292325443b0
9394113857 <81347796+9394113857@users.noreply.github.com>
2021-10-08 12:46:24 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pickle
+
+import pandas as pd
+# from sklearn.metrics import mean_squared_log_error
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+df=pd.read_csv('heart-data.csv')
+df.rename(columns={"class":"target"},inplace=True)
+df['target'].replace(['absent','present'],[0,1],inplace=True)
+df=pd.get_dummies(df)
+x=df.drop('target', axis=1)
+y=df['target']
+train_x,valid_x,train_y,valid_y=train_test_split(x,y,test_size=0.3,random_state=35)
+logr=LogisticRegression()
+logr.fit(train_x,train_y)
+#new_data=np.array(new_data,dtype='int64')
+#new_data=new_data.reshape(1,13)
+#xnew_data=pd.DataFrame(new_data)
+pickle.dump(logr,open('model.pkl','wb'))
+model=pickle.load(open('model.pkl','rb'))
+result=model.predict(valid_x)
+newdata=valid_x.head(1)
+print(newdata)
+result2=model.predict(newdata)
+print(result)
+print(result2)
+    
+
